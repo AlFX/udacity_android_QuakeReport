fab36abf9969999be07d2e355a0f0aa7735a5c22
AlFX <alessio.fanciulli.oxilia@gmail.com>
2017-07-16 22:29:00 +0100

    integrate HTML request code

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,68 +15,105 @@
  */
 package com.example.android.quakereport;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import android.content.Intent;
-import android.net.Uri;
-import android.view.View;
-import android.widget.AdapterView;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    private static final String USGS_REQUEST_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
+
+    /* when we get to the onPostExecute() method, we need to update the ListView.
+     * The only way to update the contents of the list is to update the data set within
+     * the EarthquakeAdapter. To access and modify the instance of the EarthquakeAdapter,
+     * we need to make it a global variable in the EarthquakeActivity. */
+
+    /* Adapter for the list of earthquakes */
+    private EarthquakeAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        /*ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake("6.66", "Vatican City", "June, 6th 2006"));
-        earthquakes.add(new Earthquake("6.66", "Napoli", "June, 6th 2006"));
-        earthquakes.add(new Earthquake("6.66", "Palermo", "June, 6th 2006"));
-        earthquakes.add(new Earthquake("6.66", "San Francisco", "June, 6th 2006"));
-        earthquakes.add(new Earthquake("6.66", "London", "June, 6th 2006"));
-        earthquakes.add(new Earthquake("6.66", "Tokyo", "June, 6th 2006"));
-        earthquakes.add(new Earthquake("6.66", "Mexico City", "June, 6th 2006"));
-        earthquakes.add(new Earthquake("6.66", "Moscow", "June, 6th 2006"));
-        earthquakes.add(new Earthquake("6.66", "Rio de Janeiro", "June, 6th 2006"));
-        earthquakes.add(new Earthquake("6.66", "Paris", "June, 6th 2006"));*/
-
-        /*instead of manually populating the ArrayList, we call extractEarthquakes
-        * from QueryUtils.java module which converts a JSONObjects into something
-        * manageable by Java*/
-        final ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
-
         // Find a reference to the ListView in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
-        // Create a new ArrayAdapter of earthquakes
-        final EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
+        // Create a new adapter that takes an empty list of earthquakes as input
+        mAdapter = new EarthquakeAdapter(this, new ArrayList<Earthquake>());
 
-        // Set the adapter on the ListView, so the list can be populated in the user interface
-        earthquakeListView.setAdapter(adapter);
+
+        // Set the adapter on the ListView, so the list can be populated
+        // in the user interface
+        earthquakeListView.setAdapter(mAdapter);
 
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 // Find the current earthquake that was clicked on
-                Earthquake currentEarthquake = adapter.getItem(position);
+                Earthquake currentEarthquake = mAdapter.getItem(position);
 
-
+                // Convert the String URL into a URI object
+                // (to pass into the Intent constructor)
                 Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
 
-
+                // Create a new intent to view the earthquake URI
                 Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
 
-
+                // Send the intent to launch a new activity
                 startActivity(websiteIntent);
             }
 
         });
+
+        // Start the AsyncTask to fetch the earthquake data
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
+
     }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Earthquake>> {
+
+        /* this method runs on a background thread and performs the network request.
+        * We should not update the UI from a background thread, so we return a list of
+        * Earthquakes as the result */
+        @Override
+        protected List<Earthquake> doInBackground(String... urls) {
+            // don't perform the request if there are no URLs or the first URL is null
+            if (urls.length < 1 || urls [0] == null) {
+                return null;
+            }
+            List<Earthquake> result = QueryUtils.fetchEarthquakeData(urls[0]);
+            return result;
+        }
+
+        /* this method runs on the main UI thread after the background work has been
+        * completed. It receives as input the return value from the doInBackground()
+        * method. First we clear out the adapter, toe get rid of earthquake data from
+        * a previous query to USGS. Then we update the adapter with the new list of
+        * earthquakes, which will trigger the ListView to re-populate its list items.*/
+        @Override
+        protected void onPostExecute(List<Earthquake> data) {
+
+            // Clear the adapter of previous earthquake data
+            mAdapter.clear();
+
+            /* If there is a valid list of Earthquakes, the add them to the adapter's
+            data set. This will trigger the ListView to update. */
+            if (data != null && !data.isEmpty()) {
+                mAdapter.addAll(data);
+            }
+
+        }
+    }
+
 }
